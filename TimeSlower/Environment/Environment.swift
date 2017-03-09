7a536fdb8ab6946eefb67fd20d9ee230ea1f2c7e
Alex Shcherbakov <alexander@prolificinteractive.com>
2017-03-09 09:05:32 -0500

    Initial commit

---
+++ TimeSlower/Environment/Environment.swift
@@ -0,0 +1,32 @@
+//
+//  Environment.swift
+//  TimeSlower
+//
+//  Created by Alex Shcherbakov on 3/9/17.
+//  Copyright © 2017 Alex Shcherbakov. All rights reserved.
+//
+
+import Foundation
+
+internal protocol Environment {
+
+    var router: Router { get }
+
+}
+
+internal final class AppEnvironment: Environment {
+
+    let router: Router = AppRouter()
+
+}
+
+/// Router.
+internal protocol Router {
+
+}
+
+internal struct AppRouter: Router {
+
+}
+
+
